34e8731965137bbac9a988559fad6f51eff3255e
darksun <lujun9972@gmail.com>
2015-10-06 08:20:20 +0800

    修改说明

--- elake.el
+++ elake.el
@@ -1,9 +1,29 @@
 #! /usr/bin/emacs --script
 ;;; elake.el --- elisp make
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Keywords: lisp, make
 
+;; This file is NOT part of GNU Emacs.
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; elake's code can be found here:
+;;   http://github.com/lujun9972/elake
 ;;; Commentary:
-;; 
+;; elake is an elisp make
 
 (require 'cl)
 (require 'subr-x)
